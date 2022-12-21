6db65a5bb5a3f3c9520f6949dc7569ec8858073f
norikois <33947244+norikois@users.noreply.github.com>
2022-12-22 08:45:16 +0900

    サンプル作成
    サンプル作成

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Infragistics.Windows.Editors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KB10130_XamEditors_DisableSpinning;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void xamNumericEditor1_ExecutingCommand(object sender, Infragistics.Windows.Controls.Events.ExecutingCommandEventArgs e)
+    {
+        if (e.Command == MaskedEditorCommands.SpinUp || e.Command == MaskedEditorCommands.SpinDown)
+        {
+            e.Cancel = true;
+        }
+    }
+}
